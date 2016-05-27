e243751b80517009fc5f13719e5e5471958be068
jnpj <jnjermain@gmail.com>
2016-05-27 15:32:17 +0100

    Improve page speed by adding async tag to js file, optimizing images, adding media tag to css print link, and moving scripts to bottom of body.

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var pizzaImageList = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(pizzaImageList[1], size);
+    var newwidth = (pizzaImageList[1].offsetWidth + dx) + 'px';
+    for (var i = 0; i < pizzaImageList.length; i++) {
+      pizzaImageList[i].style.width = newwidth;
     }
   }
 
